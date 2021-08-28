94157be37d821c1f22d735a75bfa1d70431d7e52
Phil Pluckthun <phil@kitten.sh>
2021-08-28 23:51:12 +0100

    Move demo to scripts/buildenv and activate workspaces

--- scripts/buildenv/vite.alias.config.js
+++ scripts/buildenv/vite.alias.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import preact from "@preact/preset-vite";
+import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
   resolve: {
     alias: {
-      graphql: path.resolve('..', './dist')
-    }
+      graphql: path.resolve('..', '..', './dist'),
+    },
   },
   build: {
-    outDir: './dist-lite'
-  }
+    outDir: './dist-lite',
+  },
 });
--- scripts/buildenv/vite.config.js
+++ scripts/buildenv/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import preact from "@preact/preset-vite";
+import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
   build: {
-    outDir: './dist-graphql'
-  }
+    outDir: './dist-graphql',
+  },
 });
