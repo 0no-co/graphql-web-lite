a15b12ca6c2021192f7fb2b0b8b07736c4b067e4
Phil Pluckthun <phil@kitten.sh>
2021-08-29 01:20:28 +0100

    Add upstream tests for visitor

--- alias/language/visitor.mjs
+++ alias/language/visitor.mjs
@@ -17,40 +17,49 @@ export function visit(node, visitor) {
 
   function traverse(node, key, parent) {
     let hasEdited = false;
-    let result;
 
-    result = callback(node, key, parent, false);
-    if (result === false) {
-      return;
-    } else if (result && typeof result.kind === 'string') {
-      hasEdited = true;
-      node = result;
+    const resultEnter = callback(node, key, parent, false);
+    if (resultEnter === false) {
+      return node;
+    } else if (resultEnter === null) {
+      return null;
+    } else if (resultEnter && typeof resultEnter.kind === 'string') {
+      hasEdited = resultEnter !== node;
+      node = resultEnter;
     }
 
-    ancestors.push(node);
-    const copy = { ...node };
+    if (parent) ancestors.push(parent);
 
+    let result;
+    const copy = { ...node };
     for (const nodeKey in node) {
-      let value = node[nodeKey];
       path.push(nodeKey);
+      let value = node[nodeKey];
       if (Array.isArray(value)) {
-        value = value.slice();
+        const newValue = [];
         for (let index = 0; index < value.length; index++) {
           if (value[index] != null && typeof value[index].kind === 'string') {
+            ancestors.push(node);
             path.push(index);
-            result = traverse(value[index], index, node);
+            result = traverse(value[index], index, value);
             path.pop();
-            if (result !== undefined) {
-              value[index] = result;
+            ancestors.pop();
+            if (result === undefined) {
+              newValue.push(value[index]);
+            } else if (result === null) {
               hasEdited = true;
+            } else {
+              hasEdited = hasEdited || result !== value[index];
+              newValue.push(result);
             }
           }
         }
+        value = newValue;
       } else if (value != null && typeof value.kind === 'string') {
         result = traverse(value, nodeKey, node);
         if (result !== undefined) {
+          hasEdited = hasEdited || value !== result;
           value = result;
-          hasEdited = true;
         }
       }
 
@@ -58,16 +67,20 @@ export function visit(node, visitor) {
       if (hasEdited) copy[nodeKey] = value;
     }
 
-    ancestors.pop();
-
-    if (hasEdited) node = copy;
-    result = callback(node, key, parent, true);
-    return hasEdited && result === undefined ? node : result;
+    if (parent) ancestors.pop();
+    const resultLeave = callback(node, key, parent, true);
+    if (resultLeave !== undefined) {
+      return resultLeave;
+    } else if (resultEnter !== undefined) {
+      return resultEnter;
+    } else {
+      return hasEdited ? copy : node;
+    }
   }
 
   try {
     const result = traverse(node);
-    return result !== undefined ? result : node;
+    return result !== undefined && result !== false ? result : node;
   } catch (error) {
     if (error !== BREAK) throw error;
     return node;
