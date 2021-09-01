3439ea2055b755263a05428759181a80c13ac7dd
jdecroock <decroockjovi@gmail.com>
2021-09-01 20:25:58 +0200

    stable filenames

--- scripts/buildenv/vite.config.js
+++ scripts/buildenv/vite.config.js
@@ -3,7 +3,18 @@ import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
+  rollupOutputOptions: {
+    chunkFileNames: '[name].js',
+    entryFileNames: '[name].js',
+  },
   build: {
     outDir: './dist-graphql',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
   },
 });
