bc8b44837591e9d105f42634aec4a862d0dc2be0
jdecroock <decroockjovi@gmail.com>
2023-03-21 09:40:01 +0100

    fix lint

--- scripts/babel/transformDevAssert.mjs
+++ scripts/babel/transformDevAssert.mjs
@@ -30,8 +30,7 @@ const plugin = ({ template, types: t }) => {
               p.parentPath.parentPath.node.body.length === 1 &&
               p.parentPath.parentPath.node.body[0] === path.parentPath.node &&
               t.isIfStatement(p.parentPath.parentPath.parentPath.node) &&
-              p.parentPath.parentPath.parentPath.node.consequent ===
-                p.parentPath.parentPath.node &&
+              p.parentPath.parentPath.parentPath.node.consequent === p.parentPath.parentPath.node &&
               !p.parentPath.parentPath.node.alternate
             ) {
               p = p.parentPath.parentPath.parentPath;
