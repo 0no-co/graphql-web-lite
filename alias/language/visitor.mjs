a8ec0b563a5253965f59f2e56beaa8260485a6f5
Phil Pluckthun <phil@kitten.sh>
2023-03-23 12:05:11 +0000

    Reformat visitInParallel

--- alias/language/visitor.mjs
+++ alias/language/visitor.mjs
@@ -18,12 +18,13 @@ export function getVisitFn(visitor, kind, isLeaving) {
 
 export function visitInParallel(visitors) {
   const skipping = new Array(visitors.length).fill(null);
-  const mergedVisitor = Object.create(null);
+  const mergedVisitor = {};
 
-  for (const kind of Object.values(Kind)) {
+  for (const kindName in Kind) {
+    const kind = Kind[kindName];
     let hasVisitor = false;
-    const enterList = new Array(visitors.length).fill(undefined);
-    const leaveList = new Array(visitors.length).fill(undefined);
+    const enterList = new Array(visitors.length).fill();
+    const leaveList = new Array(visitors.length).fill();
 
     for (let i = 0; i < visitors.length; ++i) {
       const { enter, leave } = getEnterLeaveForKind(visitors[i], kind);
@@ -32,44 +33,40 @@ export function visitInParallel(visitors) {
       leaveList[i] = leave;
     }
 
-    if (!hasVisitor) {
-      continue;
+    if (hasVisitor) {
+      mergedVisitor[kind] = {
+        enter(...args) {
+          const node = args[0];
+          for (let i = 0; i < visitors.length; i++) {
+            if (!skipping[i]) {
+              const result = enterList[i] && enterList[i].apply(visitors[i], args);
+              if (result === false) {
+                skipping[i] = node;
+              } else if (result === BREAK) {
+                skipping[i] = BREAK;
+              } else if (result !== undefined) {
+                return result;
+              }
+            }
+          }
+        },
+        leave(...args) {
+          const node = args[0];
+          for (let i = 0; i < visitors.length; i++) {
+            if (!skipping[i]) {
+              const result = leaveList[i] && leaveList[i].apply(visitors[i], args);
+              if (result === BREAK) {
+                skipping[i] = BREAK;
+              } else if (result !== undefined && result !== false) {
+                return result;
+              }
+            } else if (skipping[i] === node) {
+              skipping[i] = null;
+            }
+          }
+        },
+      };
     }
-
-    const mergedEnterLeave = {
-      enter(...args) {
-        const node = args[0];
-        for (let i = 0; i < visitors.length; i++) {
-          if (skipping[i] === null) {
-            const result = enterList[i] && enterList[i].apply(visitors[i], args);
-            if (result === false) {
-              skipping[i] = node;
-            } else if (result === BREAK) {
-              skipping[i] = BREAK;
-            } else if (result !== undefined) {
-              return result;
-            }
-          }
-        }
-      },
-      leave(...args) {
-        const node = args[0];
-        for (let i = 0; i < visitors.length; i++) {
-          if (skipping[i] === null) {
-            const result = leaveList[i] && leaveList[i].apply(visitors[i], args);
-            if (result === BREAK) {
-              skipping[i] = BREAK;
-            } else if (result !== undefined && result !== false) {
-              return result;
-            }
-          } else if (skipping[i] === node) {
-            skipping[i] = null;
-          }
-        }
-      },
-    };
-
-    mergedVisitor[kind] = mergedEnterLeave;
   }
 
   return mergedVisitor;
