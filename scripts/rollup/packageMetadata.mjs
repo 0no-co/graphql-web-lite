60545ebb30423a64878484774d6e424965dbf050
Phil Pluckthun <phil@kitten.sh>
2023-03-23 13:16:13 +0000

    Update build process to remove subfolder

--- scripts/rollup/packageMetadata.mjs
+++ scripts/rollup/packageMetadata.mjs
@@ -4,7 +4,6 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
 const rootPkg = require('../../package.json');
-const gqlPkg = require('graphql/package.json');
 const parsedVersion = semver.parse(rootPkg.version);
 
 const versionInfo = {
@@ -19,20 +18,3 @@ export const version = `
 export const version = ${JSON.stringify(parsedVersion.raw)};
 export const versionInfo = ${JSON.stringify(versionInfo)};
 `.trim();
-
-export const packageMetadata = JSON.stringify(
-  {
-    ...gqlPkg,
-    private: undefined,
-    publishConfig: undefined,
-    name: 'graphql-web-lite',
-    version: rootPkg.version,
-    sideEffects: false,
-    homepage: rootPkg.homepage,
-    bugs: rootPkg.bugs,
-    repository: rootPkg.repository,
-    keywords: rootPkg.keywords,
-  },
-  null,
-  2
-);
